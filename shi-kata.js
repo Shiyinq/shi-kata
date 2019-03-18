2d808aeeafc4d1b4b9c05c4b34f013bc044070e9
Shiyinq <shiyinq.github.io@gmail.com>
2019-03-18 16:34:14 +0700

    Add files via upload

---
+++ shi-kata.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+const program = require('commander')
+// const { prompt } = require('inquirer');
+const { 
+	makeFlow,
+	mfOpening,
+	mfSmalltalk,
+	mfFallback 
+} = require('./generate')
+
+program
+  .version('0.0.1')
+  .description('App untuk generate flow')
+
+program
+  .command('make-flow <file-name>')
+  .alias('mf')
+  .description('Membuat flow baru')
+  .action((name) => makeFlow(name))
+
+program
+	.command('mf-opening')
+	.alias('mfo')
+	.description('Membuat flow opening')
+	.action(() => mfOpening())
+
+program
+	.command('mf-smalltalk')
+	.alias('mfs')
+	.description('Membuat flow smalltalk')
+	.action(() => mfSmalltalk())
+
+program
+	.command('mf-fallback')
+	.alias('mff')
+	.description('Membuat flow fallback')
+	.action(() => mfFallback())
+
+program.parse(process.argv)
