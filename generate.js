8e3e49c091790551bcdb12cb5fe900f3bea08774
Shiyinq <agunpermana25@gmail.com>
2019-03-18 17:22:03 +0700

    update

--- generate.js
+++ generate.js
@@ -8,25 +8,25 @@ const {
 
 module.exports = {
 	makeFlow (name) {
-		fs.writeFile(__dirname +`\\${name}.yml`, makeFlow, function (err) {
+		fs.writeFile(process.cwd() +`\\${name}.yml`, makeFlow, function (err) {
 			if (err) throw err;
 			console.log(`Flow ${name}.yml telah dibuat`)
 		});
 	},
 	mfOpening () {
-		fs.writeFile(__dirname +`\\opening.yml`, mfOpening, function (err) {
+		fs.writeFile(process.cwd() +`\\opening.yml`, mfOpening, function (err) {
 			if (err) throw err;
 			console.log(`Flow opening.yml telah dibuat`)
 		});
 	},
 	mfSmalltalk () {
-		fs.writeFile(__dirname +`\\smalltalk.yml`, mfSmalltalk, function (err) {
+		fs.writeFile(process.cwd() +`\\smalltalk.yml`, mfSmalltalk, function (err) {
 			if (err) throw err;
 			console.log(`Flow smalltalk.yml telah dibuat`)
 		});
 	},
 	mfFallback () {
-		fs.writeFile(__dirname +`\\fallback.yml`, mfFallback, function (err) {
+		fs.writeFile(process.cwd() +`\\fallback.yml`, mfFallback, function (err) {
 			if (err) throw err;
 			console.log(`Flow fallback.yml telah dibuat`)
 		});
