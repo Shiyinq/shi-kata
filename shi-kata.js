4f533d1e80b4f9fa308ee5d237b1958ee1b7b030
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2019-03-19 13:31:55 +0700

    add new feature choose action types from list & better code with eslint

--- shi-kata.js
+++ shi-kata.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 
 const program = require('commander')
-// const { prompt } = require('inquirer');
-const { 
-	makeFlow,
-	mfOpening,
-	mfSmalltalk,
-	mfFallback,
-	generateAction
+const { prompt } = require('inquirer')
+const {
+  makeFlow,
+  mfOpening,
+  mfSmalltalk,
+  mfFallback,
+  generateAction,
+  actionList
 } = require('./generate')
 
 program
@@ -17,31 +18,39 @@ program
 program
   .command('make-flow <file-name>')
   .alias('mf')
-  .description('Membuat flow baru')
+  .description('Generate new flow')
   .action((name) => makeFlow(name))
 
 program
-	.command('mf-opening')
-	.alias('mfo')
-	.description('Membuat flow opening')
-	.action(() => mfOpening())
+  .command('mf-opening')
+  .alias('mfo')
+  .description('Generate flow opening')
+  .action(() => mfOpening())
 
 program
-	.command('mf-smalltalk')
-	.alias('mfs')
-	.description('Membuat flow smalltalk')
-	.action(() => mfSmalltalk())
+  .command('mf-smalltalk')
+  .alias('mfs')
+  .description('Generate flow smalltalk')
+  .action(() => mfSmalltalk())
 
 program
-	.command('mf-fallback')
-	.alias('mff')
-	.description('Membuat flow fallback')
-	.action(() => mfFallback())
+  .command('mf-fallback')
+  .alias('mff')
+  .description('Generate flow fallback')
+  .action(() => mfFallback())
 
 program
-	.command('action <nameAction>')
-	.alias('act')
-	.description(`Membuat Action Type [text, text_data, image, button, carousel, imagemap, sticker, video, audio, location, quick_reply, schedule, api]`)
-	.action((nameAction) => console.log(generateAction(nameAction)))
+  .command('action-list')
+  .alias('actl')
+  .description('Generate Action Types from list')
+  .action(() => {
+    prompt(actionList).then(({ actionTypes }) => console.log(generateAction(actionTypes)))
+  })
+
+program
+  .command('action <nameAction>')
+  .alias('act')
+  .description(`Generate Action Types`)
+  .action((nameAction) => console.log(generateAction(nameAction)))
 
 program.parse(process.argv)
