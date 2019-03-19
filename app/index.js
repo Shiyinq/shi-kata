e7867ae361e892cb85b4611068e31dfa2ea630ee
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2019-03-19 11:48:46 +0700

    add new feature, generator action types

--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
-const makeFlow = require('./flow/makeFlow')
-const mfOpening = require('./flow/mfOpening')
-const mfSmalltalk = require('./flow/mfSmalltalk')
-const mffallback = require('./flow/mffallback')
+const makeFlow = require('./flows/makeFlow')
+const mfOpening = require('./flows/mfOpening')
+const mfSmalltalk = require('./flows/mfSmalltalk')
+const mffallback = require('./flows/mffallback')
+const actionTypes = require('./actions/action')
 
 module.exports = {
 	makeFlow,
 	mfOpening,
 	mfSmalltalk,
-	mffallback
+	mffallback,
+	actionTypes
 }
